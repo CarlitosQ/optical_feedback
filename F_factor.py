ed1b01f4725e70b8d374ee90d832fca6ca8af6e9
Qian <tqian@fe.hhi.de>
2018-08-20 17:38:55 +0200

    Monday before leaving

--- F_factor.py
+++ F_factor.py
@@ -12,9 +12,9 @@ import timeit
 
 start = timeit.default_timer()
 
-#=========================================================================
+# =========================================================================
 # # Define basic paramters
-#=========================================================================
+# =========================================================================
 c = 3e8
 n_gain = 3.2
 n_poly = 1.46
@@ -28,15 +28,15 @@ L_ext = np.linspace(0, 170e-3, 100000)
 alpha = -2
 WL = np.linspace(1540e-9, 1570e-9, 10000)
 
-#=========================================================================
+# =========================================================================
 # Define r_ext
-#=========================================================================
+# =========================================================================
 # r_ext = 0.0078        # weak
 # r_ext = 0.8        # very strong
 r_ext = (n_poly - n_air) / (n_poly + n_air)   # 0.183673469388
 # r_ext = 0.9
 R_ext = np.square(r_ext)
-print ("R_ext = %s" % float('%.4g' % R_ext))
+print("R_ext = %s" % float('%.4g' % R_ext))
 
 R_bragg = 0.3
 r_bragg = np.sqrt(R_bragg)
@@ -46,27 +46,27 @@ tau_L = 2 * (n_gain * L_gain + n_poly * L_poly) / c
 # print ("tau_L= %s" % float('%.4g' % tau_c))
 
 ratio = R_ext / R_bragg
-print ("R_ext/R_bragg= %s" % float('%.4g' % ratio))
+print("R_ext/R_bragg= %s" % float('%.4g' % ratio))
 
 kappa_ext = float(r_ext / r_bragg * (1 - np.square(r_bragg)))
-print ("kappa_ext= %s" % float('%.4g' % kappa_ext))
+print("kappa_ext= %s" % float('%.4g' % kappa_ext))
 
 # kappa = (1 - r_bragg) / tau_L * np.sqrt(r_ext / r_bragg)
 # print ("kappa= %s" % ('%.6e' % Decimal(str(kappa))))
 
 X = tau_ext / tau_L * kappa_ext
-#=========================================================================
+# =========================================================================
 # plt.figure(2)
 # plt.plot(L_ext * 1000, X, 'r', label="$\kappa_{ext}$ %s, $R_{ext}$ %s" %
 #          (round(kappa_ext, 3), round(R_ext, 5)))
-#=========================================================================
+# =========================================================================
 # C = X * np.sqrt(1 + np.square(alpha))
 # print ("C= %s" % C)
 
 f_r = 9e9   # relaxation frequency 9 GHz
 # seperation of short and long cavity, in mm
 L_fr = c / (2 * n_poly * f_r) * 1000
-print ("L_fr= %s mm" % L_fr)
+print("L_fr= %s mm" % L_fr)
 
 """ 1. Weak feedback condition """
 """ 1.1 r of pol and air interface """
@@ -160,12 +160,12 @@ print ("L_fr= %s mm" % L_fr)
 
 """ 3 Combined plot """
 F_factor = []
-#=========================================================================
+# =========================================================================
 # Define C_limit: for single mode, C is smaller than 1 or 3pi/2
-#=========================================================================
+# =========================================================================
 C_limit = 1
 # C_limit = 0.977 * 3 * np.pi / 2
-print ("C_limit= %s" % C_limit)
+print("C_limit= %s" % C_limit)
 
 for l_ext in L_ext:
     tau_EXT = 2 * n_poly * l_ext / c
@@ -198,15 +198,15 @@ if kappa_ext > 0.1 or R_ext > 0.1:
     plt.plot(L_ext * 1000, F_factor, 'r', label="$\kappa_{ext}$ %s, $R_{ext}$ %s" %
              (round(kappa_ext, 3), round(R_ext, 5)))
     plt.title('Strong condition, $F^2$ factor, mirror / polymer interface')
-    np.savetxt('F_factor_Strong_kappa%s.txt' % round(kappa_ext, 3), np.transpose(
-        [(L_ext * 1000), F_factor]), fmt='%1.8e', delimiter='\t')
+#     np.savetxt('F_factor_Strong_kappa%s.txt' % round(kappa_ext, 3), np.transpose(
+#         [(L_ext * 1000), F_factor]), fmt='%1.8e', delimiter='\t')
 elif kappa_ext < 0.01:
     plt.plot(L_ext * 1000, F_factor, 'b', label="$\kappa_{ext}$ %s, $R_{ext}$ %s" %
              (round(kappa_ext, 3), round(R_ext, 5)))
 #     plt.title('Weak condition, $F^2$ factor, polymer cladding / polymer interface')
     plt.title('Weak condition, $F^2$ factor, mirror / polymer interface')
-    np.savetxt('F_factor_Weak_kappa%s.txt' % round(kappa_ext, 3), np.transpose(
-        [(L_ext * 1000), F_factor]), fmt='%1.8e', delimiter='\t')
+#     np.savetxt('F_factor_Weak_kappa%s.txt' % round(kappa_ext, 3), np.transpose(
+#         [(L_ext * 1000), F_factor]), fmt='%1.8e', delimiter='\t')
 
 plt.axvline(x=L_fr, linestyle='dashed', color='b',
             label="$\L_{f_r}$=%s" % round(L_fr, 3))
