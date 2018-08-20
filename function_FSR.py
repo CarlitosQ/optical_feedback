c225e991565232f89861da9fd6a3c193872eac0f
Qian <tqian@fe.hhi.de>
2018-08-20 16:02:04 +0200

    monday afternoon

--- function_FSR.py
+++ function_FSR.py
@@ -15,8 +15,9 @@ freq_center = c / (WL_D)
 
 L_grat = 699.4075 * 1e-6
 L_grat_eff = L_grat / 2
-L_poly = (180 + 190 + 155) * 1e-6 + L_grat_eff
 L_wg = (180 + 190 + 155) * 1e-6
+L_poly = L_wg + L_grat_eff
+
 
 r1 = np.sqrt(0.9)
 r_1 = 0.99
@@ -25,9 +26,11 @@ n_gain = 3.2
 n_air = 1.
 n_poly = 1.46
 
+
 def FreeSpectralRange(L_gain, L_ext):
-    FreeSpectralRange = (c / (2 * (n_gain * L_gain*1e-6 + n_poly * (L_poly + L_ext*1e-6))))/1e9
-    return FreeSpectralRange
+    FSR = (c / (2 * (n_gain * L_gain * 1e-6 + n_poly * (L_poly + L_ext * 1e-6)))) / 1e9
+    return FSR
+
 
 FSR = FreeSpectralRange(300, 0)
 FSR = FreeSpectralRange(500, 0)
